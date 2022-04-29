106405f0cbfd8ed52bdcb8ac77174f296f5e129c
ANIKSURODHAR <99689983+ANIKSURODHAR@users.noreply.github.com>
2022-04-30 03:41:38 +0600

    Update Lazy Chef .c

--- Lazy Chef .c
+++ Lazy Chef .c
@@ -1,3 +1,10 @@
+/*Question:Chef is a very lazy person. Whatever work is supposed to be finished in x units of time, he finishes it in m∗x units of time. But there is 
+always a limit to laziness, so he delays the work by at max d units of time. Given x,m,d, find the maximum time taken by Chef to complete the work.
+Input: First line will contain T, number of testcases. Then the testcases follow.
+Each testcase contains a single line of input, three integers x,m,d.
+Output:
+For each testcase, output in a single line answer to the problem.*/
+
 /*Author Name:Anik sutrodhar
          C.S.E
                             */
